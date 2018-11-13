5c8cd8aedbefccef4927de6b275244a52e5251d8
bjoernranft <externer.dl.ranft@muenchen.de>
2018-11-13 14:04:25 +0100

    Function-Interface Konstanten aufgeräumt.

---
+++ src/main/java/de/muenchen/allg/itd51/wollmux/core/functions/FunctionConstants.java
@@ -0,0 +1,10 @@
+package de.muenchen.allg.itd51.wollmux.core.functions;
+
+import de.muenchen.allg.itd51.wollmux.core.util.L;
+
+public class FunctionConstants
+{
+  private FunctionConstants() {}
+  
+  public static final String ERROR = L.m("!¤£!FEHLERHAFTE DATEN!¤£!");
+}

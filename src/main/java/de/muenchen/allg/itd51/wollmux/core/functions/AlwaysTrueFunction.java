bc258e25761303d22f8055278969ed861e785c80
Daniel Sikeler <daniel.sikeler@muenchen.de>
2018-11-23 11:30:27 +0100

    move functions to wollmux-core

---
+++ src/main/java/de/muenchen/allg/itd51/wollmux/core/functions/AlwaysTrueFunction.java
@@ -0,0 +1,30 @@
+package de.muenchen.allg.itd51.wollmux.core.functions;
+
+import java.util.Collection;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class AlwaysTrueFunction implements Function
+{
+  @Override
+  public String[] parameters()
+  {
+    return ArrayUtils.EMPTY_STRING_ARRAY;
+  }
+
+  @Override
+  public void getFunctionDialogReferences(Collection<String> set)
+  {}
+
+  @Override
+  public String getString(Values parameters)
+  {
+    return "true";
+  }
+
+  @Override
+  public boolean getBoolean(Values parameters)
+  {
+    return true;
+  }
+}

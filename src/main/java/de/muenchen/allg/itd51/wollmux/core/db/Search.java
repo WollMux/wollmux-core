a21fb7e86aa3a8f631d47f2b3bc79ee5ef727298
bjoernranft <externer.dl.ranft@muenchen.de>
2019-04-04 13:28:55 +0200

    Review Changes.

--- src/main/java/de/muenchen/allg/itd51/wollmux/core/db/Search.java
+++ src/main/java/de/muenchen/allg/itd51/wollmux/core/db/Search.java
@@ -139,8 +139,7 @@ public class Search
         String datasetValue = dataset.get(columnName);
         
         if ((ldapDSValue == null && datasetValue != null && !datasetValue.isEmpty())
-            || ldapDSValue != null && datasetValue != null
-                && !ldapDSValue.equals(datasetValue))
+            || (ldapDSValue != null && datasetValue != null && !ldapDSValue.equals(datasetValue)))
         {
           hasChanged = true;
           break;

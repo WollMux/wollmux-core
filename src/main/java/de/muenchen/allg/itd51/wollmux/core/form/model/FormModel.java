a8affcd3877c617bf88f09257d1427bd487e9979
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-01-23 08:35:09 +0100

    add tests for visibility of controls

--- src/main/java/de/muenchen/allg/itd51/wollmux/core/form/model/FormModel.java
+++ src/main/java/de/muenchen/allg/itd51/wollmux/core/form/model/FormModel.java
@@ -307,6 +307,13 @@ public class FormModel
     return visiblities.get(groupId);
   }
 
+  /**
+   * Get a control.
+   *
+   * @param controlId
+   *          The id of the control.
+   * @return The control or null if there is no control with this id.
+   */
   public Control getControl(String controlId)
   {
     return formControls.get(controlId);
